53e6e2e34f62d14279c64a4014261f7d8d5ab730
Devilpacks <mangyr97@mail.ru>
2021-11-19 20:49:16 +0300

    Base script

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const dotenv = require('dotenv')
-const postgres = require('postgres')
-// const fetch = require('node-fetch')
+const dotenv = require('dotenv');
+const crypto = require('crypto')
 
-console.log(dotenv);
+const postgres = require('postgres');
+const fetch = require('node-fetch');
+
+
+dotenv.config();
+
+const api = process.env.API;
+const secret = process.env.SECRET
+console.log();
+const endpoint = 'https://api.binance.com'
+const path = '/sapi/v1/accountSnapshot'
+
+const request = async (api, secret, endpoint, path) => {
+    const options = {headers: {}};
+    let nonce = Date.now().toString();
+    let dataAndNonce = 'timestamp=' + nonce ;
+    let signature = crypto.createHmac('sha256', secret).update(dataAndNonce).digest('hex');
+    message = dataAndNonce + '&signature=' + signature ;
+    options.headers['X-MBX-APIKEY'] = api;
+    const makeRequest = await fetch(`${endpoint}${path}?${message}`, options)
+                .then(response => response.json())
+                .then(json => console.log(json))
+}
+
+request(api, secret, endpoint, path) // TODO: why output is - { code: -1022, msg: 'Signature for this request is not valid.' }
