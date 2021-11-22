76fbc43eea71d1da39e502732a0422b25cfe3d75
Devilpacks <mangyr97@mail.ru>
2021-11-22 19:09:29 +0300

    Naming fix

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const dotenv = require('dotenv');
-const crypto = require('crypto')
 
 const postgres = require('postgres');
+
 const fetch = require('node-fetch');
+const crypto = require('crypto')
 
 
 dotenv.config();
 
-const api = process.env.API;
-const secret = process.env.SECRET
-console.log();
+const apiKey = process.env.APIKEY;
+const secretKey = process.env.SECRET
 const endpoint = 'https://api.binance.com'
 const path = '/sapi/v1/accountSnapshot'
+const parameters = 'type=SPOT'
 
-const request = async (api, secret, endpoint, path) => {
-    const options = {headers: {}};
-    let nonce = Date.now().toString();
-    let dataAndNonce = 'type=SPOT&' + 'timestamp=' + nonce ;
-    let signature = crypto.createHmac('sha256', secret).update(dataAndNonce).digest('hex');
-    message = dataAndNonce + '&signature=' + signature;
-    options.headers['X-MBX-APIKEY'] = api;
-    const makeRequest = await fetch(`${endpoint}${path}?${message}`, options)
-                .then(response => response.json())
-                .then(json => console.log(json.snapshotVos[0].updateTime))
+const checkResponseStatus = (res) => {
+    if(res.ok){
+        return res
+    }
+    else {
+        throw new Error(`The HTTP status of the reponse: ${res.status} (${res.statusText})`)
+    }
 }
 
-request(api, secret, endpoint, path)
+
+const GETrequest = async (apiKey, secretKey, endpoint, path, parameters) => {
+    const options = {headers: {}};
+    let nonce = Date.now().toString();
+    let parametersAndNonce = parameters + ((parameters != '') ? '&' : '') + 'timestamp=' + nonce ;
+    let signature = crypto.createHmac('sha256', secretKey).update(parametersAndNonce).digest('hex');
+    message = parametersAndNonce + '&signature=' + signature;
+    options.headers['X-MBX-APIKEY'] = apiKey;
+    const makeRequest = await fetch(`${endpoint}${path}?${message}`, options)
+                .then(checkResponseStatus)
+                .then(response => response.json())
+                .then(json => console.log(json.snapshotVos[0].updateTime))
+                .catch(err => console.log(err))
+}
+
+GETrequest(apiKey, secretKey, endpoint, path, parameters)
