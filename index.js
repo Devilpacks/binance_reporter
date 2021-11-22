cb2ad51f02667ca36813e46bcc7d2cb4db9e1773
Devilpacks <mangyr97@mail.ru>
2021-11-22 16:31:40 +0300

    Done

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const path = '/sapi/v1/accountSnapshot'
 const request = async (api, secret, endpoint, path) => {
     const options = {headers: {}};
     let nonce = Date.now().toString();
-    let dataAndNonce = 'timestamp=' + nonce ;
+    let dataAndNonce = 'type=SPOT&' + 'timestamp=' + nonce ;
     let signature = crypto.createHmac('sha256', secret).update(dataAndNonce).digest('hex');
-    message = dataAndNonce + '&signature=' + signature ;
+    message = dataAndNonce + '&signature=' + signature;
     options.headers['X-MBX-APIKEY'] = api;
-    const makeRequest = await fetch(`${endpoint}${path}?type=SPOT&${message}`, options)
+    const makeRequest = await fetch(`${endpoint}${path}?${message}`, options)
                 .then(response => response.json())
-                .then(json => console.log(json))
+                .then(json => console.log(json.snapshotVos[0].updateTime))
 }
 
-request(api, secret, endpoint, path) // TODO: why output is - { code: -1022, msg: 'Signature for this request is not valid.' } добавить параметры запроса в сигнатуру
+request(api, secret, endpoint, path)
