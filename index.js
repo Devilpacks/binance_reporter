19459a7f4f9a64583983b2154d3bd9a74680d7de
Devilpacks <mangyr97@mail.ru>
2021-11-26 19:31:18 +0300

    Test end

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ dotenv.config();
 const apiKey = process.env.APIKEY;
 const secretKey = process.env.SECRET
 const endpoint = 'https://api.binance.com'
-const path = '/sapi/v1/capital/config/getall'
+const path = '/api/v3/account'
 const parameters = ''
 
 const balance = async (apiKey, secretKey, endpoint, path, parameters) => {
     try {
         const bncSnap = await bnc(apiKey, secretKey, endpoint, path, parameters)
-        console.log(...bncSnap);
+        console.log(bncSnap);
     } catch (error) {
         console.log(error);
     }
