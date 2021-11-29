a0a01c6091ca834f3d6459ac3c6d0bfc2da0bfa7
Devilpacks <mangyr97@mail.ru>
2021-11-29 21:05:07 +0300

    Test fapi

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const dotenv = require('dotenv');
 const bnc = require('./getRequest')
 const postgres = require('postgres');
-
+const fs = require('fs')
 dotenv.config();
 
-const apiKey = process.env.APIKEY;
-const secretKey = process.env.SECRET
-const endpoint = 'https://api.binance.com'
-const path = '/api/v3/account'
+const apiKey = process.env.bncKey;
+const secretKey = process.env.bncSecret
+const endpoint = 'https://fapi.binance.com'
+const path = '/fapi/v1/account'
 const parameters = ''
 
 const balance = async (apiKey, secretKey, endpoint, path, parameters) => {
     try {
         const bncSnap = await bnc(apiKey, secretKey, endpoint, path, parameters)
-        console.log(bncSnap);
+        fs.writeFileSync("bncSnap.json", JSON.stringify(bncSnap))
+        var jsonBncAccount = JSON.stringify(bncSnap);
+        var balance_bnc_usd = parseFloat(bncSnap.totalMarginBalance);
+        let gross_margin = balance_bnc_usd;
+        let used_margin = balance_bnc_usd - bncSnap.maxWithdrawAmount;
+        // let calc_margin = (balance_bnc_usd > 0) ? position_bnc_usd / bncLeverage : 0.0;
+        console.log(gross_margin, used_margin, used_margin/gross_margin, bncSnap.totalInitialMargin, bncSnap.totalMaintMargin, bncSnap.totalPositionInitialMargin, bncSnap.totalOpenOrderInitialMargin);
     } catch (error) {
         console.log(error);
     }
